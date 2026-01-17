de82b4bec7b9bc7a2d825c31ba44792ddd0b40a0
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2026-01-17 14:51:03 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Winterhold_College_Course_Registration_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
